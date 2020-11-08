225c429b6f4331fbc2e9f503937595cdc74ae6bb
Andreas Johansson <andreas@ndrs.xyz>
2020-11-08 11:14:09 +0100

    Add a skeleton chi-based server with kong argparsing

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module gitlab.com/ndreas/notify-api
 
 go 1.15
+
+require (
+	github.com/alecthomas/kong v0.2.11
+	github.com/go-chi/chi v4.1.2+incompatible
+	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/alecthomas/kong"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+var CLI struct {
+	Listen string `arg env:"NOTIFY_API_LISTEN" help:"Where to listen" default:":3000"`
+}
 
 func main() {
-	fmt.Println("Notify API ⚡")
+	log.Println("Notify API ⚡")
+	kong.Parse(&CLI)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Notify API ⚡"))
+	})
+
+	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNoContent)
+	})
+
+	log.Printf("Listening on %s", CLI.Listen)
+	err := http.ListenAndServe(CLI.Listen, r)
+	if err != nil {
+		log.Fatalf("ListenAndServe failed: %v", err)
+	}
 }
