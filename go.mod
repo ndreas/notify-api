1c07be6c1ef0c4f00dd18d3e448a7c472b372d6f
Andreas Johansson <andreas@ndrs.xyz>
2020-11-09 08:42:57 +0100

    Add a simple endpoint that creates notifications

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.15
 
 require (
 	github.com/alecthomas/kong v0.2.11
+	github.com/esiqveland/notify v0.9.1
 	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/godbus/dbus/v5 v5.0.3
 	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/alecthomas/kong"
+	"github.com/esiqveland/notify"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/godbus/dbus/v5"
 )
 
 var CLI struct {
@@ -17,6 +20,23 @@ func main() {
 	log.Println("Notify API ⚡")
 	kong.Parse(&CLI)
 
+	bus, err := dbus.SessionBusPrivate()
+	if err != nil {
+		log.Fatalf("Failed to get DB session bus: %v", err)
+	}
+	defer bus.Close()
+	log.Println("Connected to dbus!")
+
+	err = bus.Auth(nil)
+	if err != nil {
+		log.Fatalf("Failed to authenticate on session bus: %v", err)
+	}
+
+	err = bus.Hello()
+	if err != nil {
+		log.Fatalf("Failed to call session bus: %v", err)
+	}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
@@ -25,12 +45,60 @@ func main() {
 	})
 
 	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+		var body struct {
+			App     string `json:"app"`
+			Title   string `json:"title"`
+			Body    string `json:"body"`
+			Expiry  int32  `json:"expiry"`
+			Urgency string `json:"urgency"`
+		}
+
+		body.App = "notify-api"
+		body.Expiry = -1
+		body.Urgency = "normal"
+
+		err := json.NewDecoder(r.Body).Decode(&body)
+		if err != nil {
+			log.Printf("Failed to parse json body: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		n := notify.Notification{
+			AppName:       body.App,
+			Summary:       body.Title,
+			Body:          body.Body,
+			ExpireTimeout: body.Expiry,
+
+			Hints: map[string]dbus.Variant{
+				"urgency": urgencyVariant(body.Urgency),
+			},
+		}
+
+		_, err = notify.SendNotification(bus, n)
+		if err != nil {
+			log.Printf("Failed to call dbus: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
 		w.WriteHeader(http.StatusNoContent)
 	})
 
 	log.Printf("Listening on %s", CLI.Listen)
-	err := http.ListenAndServe(CLI.Listen, r)
+	err = http.ListenAndServe(CLI.Listen, r)
 	if err != nil {
 		log.Fatalf("ListenAndServe failed: %v", err)
 	}
+}
+
+func urgencyVariant(u string) dbus.Variant {
+	var n uint8 = 1
+	if u == "low" {
+		n = 0
+	} else if u == "critical" {
+		n = 2
+	}
+
+	return dbus.MakeVariant(n)
 }
